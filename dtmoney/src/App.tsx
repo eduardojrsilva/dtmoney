ed1eb4df7b491aaa550f14ffdfb97e1556919432
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-04-21 16:16:07 -0300

    creating new transaction modal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransctionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransctionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransctionModal} />
       <Dashboard />
+
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransctionModal} />
+
       <GlobalStyle />
     </>
   );
